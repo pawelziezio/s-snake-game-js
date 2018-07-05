d1c24de7e6fecbf499616843080d7035c297dc04
pawelziezio <pawel.ziezio@yahoo.com>
2018-07-05 13:52:20 +0200

    snake on the way + arrow keys done

--- sript.js
+++ sript.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const cellsX = 20;
     const cellsY = 20;
 
+    //initil position
+    const snake = [ [10,10] ];
+
+    //initial heading 1 - N, 2 - E, 3 - S, 4 - w
+    let direction = 1;
+
     for( let i = 0; i < cellsY ; i++ ){
         const tr = document.createElement("tr");
         for( let j = 0; j < cellsX ; j++ ){
@@ -17,6 +23,63 @@ document.addEventListener('DOMContentLoaded', function() {
         gameBoard.appendChild(tr);
     }
 
+    // get direction
+    document.addEventListener('keydown', getDirection)
+
+    function getDirection(e){
+        switch(e.keyCode) {
+            case 37:
+                direction = 4;
+                break;
+            case 38:
+                direction = 1;
+                break;
+            case 39:
+                direction = 2;
+                break;
+            case 40:
+                direction = 3;
+                break;
+        }
+    }
+
+    function drowSnake () {
+        let snakeHeadX = snake[0][0];
+        let snakeHeadY = snake[0][1];
+
+        //new head based on the heading
+        switch(direction) {
+            case 1:
+                snakeHeadY--;
+                break;
+            case 2:
+                snakeHeadX++;
+                break;
+            case 3:
+                snakeHeadY++;
+                break;
+            case 4:
+                snakeHeadX--;
+                break;
+}
+        let newHead = [snakeHeadX, snakeHeadY];
+
+        snake.unshift(newHead);
+
+        let lastCell = snake.pop();
+        const snakeLastCell = document.querySelector(`[data-row='${lastCell[1]}'][data-col='${lastCell[0]}']`);
+        snakeLastCell.classList.remove('snake');
+
+        snake.forEach(cell => {
+            const snakeCell = document.querySelector(`[data-row='${cell[1]}'][data-col='${cell[0]}']`);
+            snakeCell.classList.add('snake')
+        })
+    };
+
+    setInterval(drowSnake, 600)
+    drowSnake()
+    // drowSnake()
 
 
-})
+
+    })
